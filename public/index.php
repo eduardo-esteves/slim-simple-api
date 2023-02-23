a5df4abd971f5cb0bcf4a262d1994bd15f127a0b
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-02-22 23:43:35 -0300

    added new route to get only one book by id

--- public/index.php
+++ public/index.php
@@ -14,24 +14,43 @@ $app = AppFactory::create();
 // Add Error Handling Middleware
 $app->addErrorMiddleware(true, false, false);
 
-$app->get('/', function(Request $req, Response $resp, $args){
+$app->get('/', function (Request $req, Response $resp, $args) {
     $resp->getBody()->write('Hello World!');
 });
 
 // all books
-$app->get('/books', function (Request $request, Response $response) {
+$app->get('/books', function (Request $req, Response $resp) {
     try {
         // picking books from database
         $db = new Database;
         $books = $db->getAll();
 
-        $response->getBody()->write(json_encode($books));
+        $resp->getBody()->write(json_encode($books));
         // custom json response
-        return $response->withHeader('Content-Type', 'application/json');
+        return $resp->withHeader('Content-Type', 'application/json');
     } catch (\Exception $e) {
         $error['err'] = $e->getMessage();
-        $response->getBody()->write(json_encode($error));
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+        $resp->getBody()->write(json_encode($error));
+        return $resp->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
+
+// get one book by id
+$app->get('/books/{id}', function (Request $req, Response $resp) {
+    try {
+        $id = $req->getAttribute('id');
+
+        // picking a book
+        $db = new Database;
+        $book = $db->findById($id);
+
+        $resp->getBody()->write(json_encode($book));
+        // custom json response
+        return $resp->withHeader('Content-Type', 'application/json');
+    } catch (\Exception $e) {
+        $error['err'] = $e->getMessage();
+        $resp->getBody()->write(json_encode($error));
+        return $resp->withHeader('Content-Type', 'application/json')->withStatus(500);
     }
 });
 
