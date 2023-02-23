52b88c24eb43e51cb4b2d0f7a3de86ccd49f40e3
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-02-22 23:42:44 -0300

    developed new method findById() for return only one data

--- src/models/Database.php
+++ src/models/Database.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\models;
+
 use PDO;
 
 require __DIR__.'/../../config/config_db.php';
@@ -12,7 +13,6 @@ class Database
     public function connect()
     {
         $connect_str = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
-
         $pdo = new PDO($connect_str, DB_USER, DB_PASSWORD);
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -23,6 +23,17 @@ class Database
     public function getAll()
     {
         $sql = "SELECT id, title, author, book_description FROM books";
+        $this->connect();
+
+        $stmt = $this->pdo->query($sql);
+
+        $this->pdo = null;
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function findById(int $id)
+    {
+        $sql = "SELECT id, title, author, book_description FROM books WHERE id = $id";
         $this->connect();
 
         $stmt = $this->pdo->query($sql);
